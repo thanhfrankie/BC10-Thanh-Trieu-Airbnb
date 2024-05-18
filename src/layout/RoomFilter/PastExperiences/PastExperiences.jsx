09cd607c1330956ca03c04fc32535039fbcdd6a5
Thanh <minhthanh233239@gmail.com>
2024-05-18 07:16:46 +0700

    update pastexp

--- src/layout/RoomFilter/PastExperiences/PastExperiences.jsx
+++ src/layout/RoomFilter/PastExperiences/PastExperiences.jsx
@@ -19,14 +19,14 @@ const PastExperiences = () => {
   }, []);
   return (
     <div>
-      <div className="w-full mx-auto flex flex-col gap-3 items-center justify-start  py-2">
-        <h2 className="font-semibold text-2xl">Past Experiences</h2>
+      <h2 className="font-semibold text-2xl">Past Experiences</h2>
+      <div className="w-full flex flex-col gap-3 items-center justify-start  py-2">
         <div className="flex gap-4 flex-wrap">
           {listLocationArr &&
             listLocationArr.map((location) => (
               <div
                 key={location.id}
-                className="RoomCard sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4 px-3 py-2 rounded-lg hover:shadow-lg  transition duration-300 ease-in-out"
+                className="RoomCard sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4  rounded-lg hover:shadow-lg  transition duration-300 ease-in-out"
               >
                 <NavLink to={`/rooms/${convertToSlug(location.tinhThanh)}`}>
                   {location.hinhAnh && (
