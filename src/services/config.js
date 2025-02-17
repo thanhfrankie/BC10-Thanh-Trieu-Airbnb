113a39749e705d17d00eb8840fe5cf379f192eee
Thanh <minhthanh233239@gmail.com>
2025-02-18 04:32:40 +0700

    update token

--- src/services/config.js
+++ src/services/config.js
@@ -4,7 +4,7 @@ import { getLocalStorage } from "../utils/util";
 export const http = axios.create({
   baseURL: "https://airbnbnew.cybersoft.edu.vn/api",
   headers: {
-    tokenCyberSoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgNDEiLCJIZXRIYW5TdHJpbmciOiIxNy8xMC8yMDI0IiwiSGV0SGFuVGltZSI6IjE3MjkxMjMyMDAwMDAiLCJuYmYiOjE3MTE2NDUyMDAsImV4cCI6MTcyOTI3MDgwMH0.eZHFmA2-LPiTU4hhprKRbSSPHBM77j9yic31O97Z0rc",
+    tokenCyberSoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAxMCIsIkhldEhhblN0cmluZyI6IjMwLzEwLzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc2MTc1NzIwMDAwMCIsIm5iZiI6MTY5ODY4NTIwMCwiZXhwIjoxNzYxNzU3MjAwfQ.R238KQt9e-rThXuVLj1cb3kh1KFNdm3eTIiaP4z3IQQ",
     token: `${getLocalStorage("user")?.token}`,
   },
   timeout: "10000",
